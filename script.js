d03ea8d788019e99bd9a2a4b1f38400439434564
enanesh <edna.ganesha.gr@gmail.com>
2022-10-27 01:31:33 -0700

    Html elements,js logic, css classes applied to html

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 
+
+
+
+//SETS TEXT TODAY DATE 
+
 var date = moment().format("[Today is: ]dddd, MMMM Do YYYY");
 $("#currentDay").text(date);
 
 
-console.log(date);
+//GETS THE DAY HOUR
 
-//var workDayHours = moment().hour(9).format("hha"); // 12H clock;
-
-console.log(workDayHours);
+var workDayHours = moment().format("hha"); // 12H clock;
 
 
-var workDayHours = ["1", "2", "3", "4", "5", "6", "7", "8"]
+console.log(typeof workDayHours);
+
+console.log(workDayHours)
+
+
+
+// BUTTON EVENT LISTENER
+
+function todosStorage() {
+
+    for (i = 1; i < 10; i++) {
+
+        $("#btn" + i).click(function () {
+            var todos = $(this).parent().attr("id");
+            localStorage.setItem(todos, JSON.stringify($("#" + todos + " textarea").val()));
+      
+        })
+
+    }
+
+
+}
+
+todosStorage()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
