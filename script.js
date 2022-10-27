18711c5f41540dfa5c46cac565701d572463a77f
enanesh <edna.ganesha.gr@gmail.com>
2022-10-27 14:02:05 -0700

    Showing localstorage, so when the user refresh the page data stays

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+var todos = $(this).parent().attr("id");
 
 
 //SETS TEXT TODAY DATE 
@@ -27,19 +27,39 @@ function todosStorage() {
 
         $("#btn" + i).click(function () {
             var todos = $(this).parent().attr("id");
-            localStorage.setItem(todos, JSON.stringify($("#" + todos + " textarea").val()));
+            localStorage.setItem(todos,($("#" + todos + " textarea").val()));
       
         })
 
     }
 
-
 }
 
+function todosShow() {
+    
+    var hours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+ 
+    for (i = 0; i <= hours.length; i++) {
+
+        var todosScreen = localStorage.getItem(hours[i]);
+
+        $("#text"+i).text(todosScreen);
+    
+
+        console.log(todosScreen);
+
+    }
+}
+   
+
 todosStorage()
+todosShow()
 
 
 
+
+// try to create the elements in the html trough js code, using jquery
+//think on how to match the hour current hour with to the hour sections. 
 
 
 
