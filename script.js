8b16505f3dd2816533f0e92cb2033bb3242224e6
enanesh <edna.ganesha.gr@gmail.com>
2022-10-28 18:55:53 -0700

    Comments added

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ function colorBlock() {
 
     
     $(".row").each(function () {
-
+        //GETS THE ID FROM THE DIV  AND CONVERTS THAT INTO A INT TO BE ABLE TO COMPARE VALUES WITH CURRENT TIME INT 
+        
         var idTime = parseInt($(this).attr("id"));
 
         //GETS THE CURRENT HOUR
         var currentTime = parseInt(moment().format("HH"));// 24H clock;
+
         //REMOVES THE CLASSES FROM THE TIME-BLOCK 
         $(this).removeClass("past present future");
 
@@ -46,8 +48,6 @@ function colorBlock() {
 }
 
 
-
-
 // BUTTON EVENT LISTENER
 
 function todosStorage() {
@@ -68,14 +68,14 @@ function todosStorage() {
 
 //GETS THE LOCAL STORAGE AND DISPLAY IT INTO THE TEXTAREA ELEMENT
 
-
 function todosShow() {
     
  
     for (i = 0; i <= hours.length; i++) {
-
+        //GETS THE DATA FROM THE LOCAL STORAGE
         var todosScreen = localStorage.getItem(hours[i]);
 
+        //SHOWS THE LOCAL AS A TEXT INTO THE TEXTARE TAG
         $("#text"+i).text(todosScreen);
     
 
@@ -86,7 +86,6 @@ function todosShow() {
     
 }
  
-
 
 todosShow()
 colorBlock()
